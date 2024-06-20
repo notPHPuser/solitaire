bf4b23c15d30736991a99ab71c9e251c03dd5ab5
notPHPuser <artem2005dm@mail.ru>
2024-06-20 03:22:27 +0300

    haha

---
+++ client/src/components/interfaces/interface.ts
@@ -0,0 +1,15 @@
+export interface card {
+  13: string;
+  12: string;
+  11: string;
+  10: string;
+  9: number;
+  8: number;
+  7: number;
+  6: number;
+  5: number;
+  4: number;
+  3: number;
+  2: number;
+  1: number;
+}
--- client/src/components/main/Main.tsx
+++ client/src/components/main/Main.tsx
@@ -1,6 +1,24 @@
 import './Main.css';
+import '../interfaces/interface';
+import { card } from '../interfaces/interface';
 
 function Main() {
+  const card: card = {
+    13: 'туз',
+    12: 'король',
+    11: 'королева',
+    10: 'валет',
+    9: 9,
+    8: 8,
+    7: 7,
+    6: 6,
+    5: 5,
+    4: 4,
+    3: 3,
+    2: 2,
+    1: 1,
+  };
+
   return (
     <>
       <div className='main'></div>
